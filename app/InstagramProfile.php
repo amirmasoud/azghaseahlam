23cafc41ebf9efc05dc2388cc8a7b07498b4a5ae
AmirMasoud <amirmasood32@ymail.com>
2015-12-06 21:53:44 +0330

    add fillable and model connection to Image/InstagramProfile models

--- app/InstagramProfile.php
+++ app/InstagramProfile.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class InstagramProfile extends Model
 {
-    //
+	/**
+	 * variable that can be mass assigned.
+     * 
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'token',
+	];
+
+    /**
+     * An Instagram Profile can have many Photos.
+     * 
+     * @return HasMany
+     */
+    public function photos()
+    {
+        return $this->hasMany('App\Photos', 'instagram_profile_token', 'token');
+    }
 }
