8e415b8adcda7ceece8601dd0980fa5acc062781
AmirMasoud <amirmasood32@ymail.com>
2015-12-06 21:12:10 +0330

    define instagram_profiles and images table Add InstagramProfile model

---
+++ database/migrations/2015_12_04_141155_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('link');
+            $table->string('low_resolution');
+            $table->string('thumbnail');
+            $table->string('high_resolution');
+            $table->text('caption_text');
+            $table->unsignedInteger('token')->unique();
+            $table->unsignedInteger('instagram_profile_token');
+            $table->foreign('instagram_profile_token')->references('token')->on('instagram_profiles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('images');
+    }
+}
