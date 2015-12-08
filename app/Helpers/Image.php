6767bd94ca8e043c5afe5624aa3bed0b87bf3f36
AmirMasoud <amirmasood32@ymail.com>
2015-12-08 15:21:29 +0330

    clean up get method in ImageContract add debugger directory, contents will igonre

--- app/Helpers/Image.php
+++ app/Helpers/Image.php
@@ -7,24 +7,27 @@ use App\Helpers\Contracts\ImageContract;
 
 class Image implements ImageContract
 {
-
+    /**
+     * Get all images
+     * @return JSON
+     */
     public function get()
     {
-        $images = ImageModel::select('id', 'standard_resolution', 'low_resolution', 'caption_text', 'link')->orderBy('id', 'desc')->simplePaginate(24);
-        $json = [];
+        $images = ImageModel::select('id', 'standard_resolution', 'low_resolution', 'caption_text', 'link')
+                            ->orderBy('id', 'desc')
+                            ->simplePaginate(24);
+
+        /**
+         * Add next/prev image id
+         */
         foreach ($images as $image) {
             $nextId = ImageModel::NextId($image->id);
             $prevId = ImageModel::prevId($image->id);
             $image->next = is_int( $nextId ) ? $nextId : 0;
             $image->prev = is_int( $prevId ) ? $prevId : 0;
-            $json[] = $image;
         }
 
         return $images;
-
-        return response()->json([]);
-        return [$images];
-        return ['images' => [$images]];
     }
 
 }
