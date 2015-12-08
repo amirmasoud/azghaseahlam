dc7feb1bf537e1415135198ede8cf10b29b7109e
AmirMasoud <amirmasood32@ymail.com>
2015-12-08 19:50:04 +0330

    change footer text, persian version

--- resources/views/theme/footer.blade.php
+++ resources/views/theme/footer.blade.php
@@ -4,7 +4,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>تمام حقوق سایت متعلق به اضغاث احلام است. 1394 &copy;</p>
                 </div>
             </div>
             <!-- /.row -->
