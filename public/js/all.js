23012c3c9a79699772e265d7acd0924e319cc050
AmirMasoud <amirmasood32@ymail.com>
2015-12-17 00:37:00 +0330

    fix spin on modal

--- public/js/all.js
+++ public/js/all.js
@@ -2781,7 +2781,8 @@ angular.module('mainCtrl', [])
 .controller('mainController', function($scope, $http, Image) {
     $scope.images = [];
     $scope.singular = [];
-    $scope.loadingImage = false;
+    $scope.loadingImageNext = false;
+    $scope.loadingImagePrev = false;
     var busy = false;
     var page = 1;
 
@@ -2802,14 +2803,14 @@ angular.module('mainCtrl', [])
 		return getImages();
 	};
 
-	$scope.openImage = function(id) {
+	$scope.openImage = function(id, direction) {
 		if (id) {
-				$scope.loadingImage = true;
+				$scope['loadingImage' + direction] = true;
 				$scope.singular.standard_resolution = '#';
 				$scope.singular.caption_text = '';
 			return Image.singular(id)
 				.then(function(result) {
-					$scope.loadingImage = false;
+					$scope['loadingImage' + direction] = false;
 					$scope.singular = result['data'];
 				});
 		}
--- resources/assets/js/controllers/mainCtrl.js
+++ resources/assets/js/controllers/mainCtrl.js
@@ -3,7 +3,8 @@ angular.module('mainCtrl', [])
 .controller('mainController', function($scope, $http, Image) {
     $scope.images = [];
     $scope.singular = [];
-    $scope.loadingImage = false;
+    $scope.loadingImageNext = false;
+    $scope.loadingImagePrev = false;
     var busy = false;
     var page = 1;
 
@@ -24,14 +25,14 @@ angular.module('mainCtrl', [])
 		return getImages();
 	};
 
-	$scope.openImage = function(id) {
+	$scope.openImage = function(id, direction) {
 		if (id) {
-				$scope.loadingImage = true;
+				$scope['loadingImage' + direction] = true;
 				$scope.singular.standard_resolution = '#';
 				$scope.singular.caption_text = '';
 			return Image.singular(id)
 				.then(function(result) {
-					$scope.loadingImage = false;
+					$scope['loadingImage' + direction] = false;
 					$scope.singular = result['data'];
 				});
 		}
